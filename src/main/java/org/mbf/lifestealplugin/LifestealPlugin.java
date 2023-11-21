364557a4422ca7efc64ca6b70e4ae46c6720dbac
MrEmmet66 <lorrenc7@gmail.com>
2023-11-21 10:26:36 +0200

    Initial commit

---
+++ src/main/java/org/mbf/lifestealplugin/LifestealPlugin.java
@@ -0,0 +1,17 @@
+package org.mbf.lifestealplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LifestealPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
